ce269636d9ea6b469d56426b89e0492c2fee848e
Sven Nilsen <bvssvni@gmail.com>
2021-05-04 17:59:22 +0200

    Added `and::assoc`

--- src/and.rs
+++ src/and.rs
@@ -6,3 +6,10 @@ use crate::*;
 pub fn commute<A: Prop, B: Prop>((f0, f1): And<A, B>) -> And<B, A> {
     (f1, f0)
 }
+
+/// `(a ∧ b) ∧ c  =>  a ∧ (b ∧ c)`
+pub fn assoc<A: Prop, B: Prop, C: Prop>(
+    ((x0, x1), x2): And<And<A, B>, C>
+) -> And<A, And<B, C>> {
+    (x0, (x1, x2))
+}
