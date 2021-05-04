3e63b345510f74e78505c0fe15f1d98e135a3941
Sven Nilsen <bvssvni@gmail.com>
2021-05-04 17:18:20 +0200

    First commit

---
+++ src/imply.rs
@@ -0,0 +1,21 @@
+//! Tactics for Logical IMPLY.
+
+use crate::*;
+
+/// `a => b  =>  ¬b => ¬a`.
+///
+/// Swap sides of implication by taking their negation.
+pub fn neg<A: Prop, B: Prop>(f: Imply<A, B>) -> Imply<Not<B>, Not<A>> {
+    Rc::new(move |x| {
+        let f = f.clone();
+        Rc::new(move |y| match x(f(y)) {})
+    })
+}
+
+/// `a => b ∧ b => c  =>  a => c`.
+pub fn transitivity<A: Prop, B: Prop, C: Prop>(
+    f: Imply<A, B>,
+    g: Imply<B, C>,
+) -> Imply<A, C> {
+    Rc::new(move |x| g(f(x)))
+}
