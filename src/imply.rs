a290fa79fe7cb8984cc00131db70071b182d3a57
Sven Nilsen <bvssvni@gmail.com>
2021-05-04 19:30:48 +0200

    Added `imply::rev_double_neg`

--- src/imply.rs
+++ src/imply.rs
@@ -43,3 +43,9 @@ pub fn double_neg<A: Prop, B: Prop>(f: Imply<A, B>, a: A) -> Imply<Not<Not<A>>, 
     let g = a.double_neg();
     Rc::new(move |x| not::double(f(g(x))))
 }
+
+/// `(¬¬a => ¬¬b) ∧ b  =>  a => b`.
+pub fn rev_double_neg<A: Prop, B: Prop>(f: Imply<Not<Not<A>>, Not<Not<B>>>, b: B) -> Imply<A, B> {
+    let g = b.double_neg();
+    Rc::new(move |x| g(f(not::double(x))))
+}
