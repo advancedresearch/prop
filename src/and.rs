fc722800162169594f0479431527b6b9242ce2a4
Sven Nilsen <bvssvni@gmail.com>
2021-05-10 17:24:17 +0200

    Improved comments and code

--- src/and.rs
+++ src/and.rs
@@ -7,14 +7,14 @@ pub fn commute<A: Prop, B: Prop>((f0, f1): And<A, B>) -> And<B, A> {
     (f1, f0)
 }
 
-/// `(a ∧ b) ∧ c  =>  a ∧ (b ∧ c)`
+/// `(a ∧ b) ∧ c  =>  a ∧ (b ∧ c)`.
 pub fn assoc<A: Prop, B: Prop, C: Prop>(
     ((x0, x1), x2): And<And<A, B>, C>
 ) -> And<A, And<B, C>> {
     (x0, (x1, x2))
 }
 
-/// `a ∧ (b ∨ c)  =>  (a ∧ b) ∨ (a ∧ c)`
+/// `a ∧ (b ∨ c)  =>  (a ∧ b) ∨ (a ∧ c)`.
 pub fn distrib<A: Prop, B: Prop, C: Prop>(
     (a, x): And<A, Or<B, C>>
 ) -> Or<And<A, B>, And<A, C>> {
@@ -24,10 +24,8 @@ pub fn distrib<A: Prop, B: Prop, C: Prop>(
     }
 }
 
-/// `¬a ∧ (a ∨ b)  =>  b`
-pub fn exc_left<A: Prop, B: Prop>(
-    (not_a, x): And<Not<A>, Or<A, B>>
-) -> B {
+/// `¬a ∧ (a ∨ b)  =>  b`.
+pub fn exc_left<A: Prop, B: Prop>((not_a, x): And<Not<A>, Or<A, B>>) -> B {
     match x {
         Left(a) => match not_a(a) {},
         Right(b) => b
