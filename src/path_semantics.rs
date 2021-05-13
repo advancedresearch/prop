635f3553b81aa83b8a181b5d0207a36189cf7d88
Sven Nilsen <bvssvni@gmail.com>
2021-05-13 14:12:27 +0200

    Removed `POrd` constraint on `assume`

--- src/path_semantics.rs
+++ src/path_semantics.rs
@@ -198,9 +198,7 @@ pub type PSemNaiveNorm<A, B, C, D> = PSemNaive<
 >;
 
 /// Assumes the core axiom safely for propositions.
-pub fn assume<A: Prop, B: Prop, C: Prop, D: Prop>() -> PSem<A, B, C, D>
-    where A: POrd<C>
-{
+pub fn assume<A: Prop, B: Prop, C: Prop, D: Prop>() -> PSem<A, B, C, D> {
     unimplemented!()
 }
 
