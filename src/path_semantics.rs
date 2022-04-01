4a2e5a2b492613e91024441e424213cd9cf0f681
Sven Nilsen <bvssvni@gmail.com>
2022-04-02 01:51:17 +0200

    Added `path_semantics::eq_true_ltrue`

--- src/path_semantics.rs
+++ src/path_semantics.rs
@@ -41,6 +41,11 @@ pub fn ty_in_right_arg<A: Prop, B: Prop, C: Prop>((ab, pord): Ty<A, B>, eq: Eq<B
     (imply::in_right_arg(ab, eq.clone()), pord.by_eq_right(eq))
 }
 
+/// `true == ltrue`.
+pub fn eq_true_ltrue<N: Nat>() -> Eq<True, LTrue<N>> {
+    (LTrue(Default::default()).map_any(), True.map_any())
+}
+
 /// `(x : a) ⋀ (y : b)  =>  ((x ⋀ y) : (a ⋀ b))`.
 pub fn ty_and<X: Prop, Y: Prop, A: Prop, B: Prop>(
     (xa, pord_xa): Ty<X, A>,
