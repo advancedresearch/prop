8db7c953d67257f71df6f67fd2993f96ad95497b
Sven Nilsen <bvssvni@gmail.com>
2022-04-20 17:47:48 +0200

    Added two proofs about Catuṣkoṭi
    - Added "catuskoti_excm_absurd"
    - Added "catuskoti_e_absurd"

--- src/existence.rs
+++ src/existence.rs
@@ -108,3 +108,89 @@ pub fn crosseq_transitivity<A: EProp, B: EProp, C: EProp>(
 ) -> CrossEq<A, C> {
     eqnn_to_crosseq(eq::transitivity(crosseq_to_eqnn(ab), crosseq_to_eqnn(bc)))
 }
+
+/// Proves that any Catuṣkoṭi with relative excluded middle is absurd.
+///
+/// A Catuṣkoṭi is a 4-value logic.
+/// For more information, see https://en.wikipedia.org/wiki/Catu%E1%B9%A3ko%E1%B9%ADi
+pub fn catuskoti_excm_absurd<A: Prop, B: Prop>(
+    catus: Not<Eq<ExcM<A>, ExcM<B>>>
+) -> False {
+    let catus2 = catus.clone();
+    let a_nexcm_b: Imply<A, Not<ExcM<B>>> =
+        Rc::new(move |a| {
+            let catus2 = catus2.clone();
+            Rc::new(move |excm_b| {
+                let eq = (excm_b.map_any(), Left(a.clone()).map_any());
+                catus2.clone()(eq)
+            })
+        });
+    let catus2 = catus.clone();
+    let na_nexcm_b: Imply<Not<A>, Not<ExcM<B>>> =
+        Rc::new(move |na| {
+            let catus2 = catus2.clone();
+            Rc::new(move |excm_b| {
+                let eq = (excm_b.map_any(), Right(na.clone()).map_any());
+                catus2.clone()(eq)
+            })
+        });
+    let a_nexcm_b2 = a_nexcm_b.clone();
+    let a_nb: Imply<A, Not<B>> =
+        Rc::new(move |a| {
+            let nexcm_b = a_nexcm_b2(a);
+            Rc::new(move |b| nexcm_b(Left(b)))
+        });
+    let a_nnb: Imply<A, Not<Not<B>>> =
+        Rc::new(move |a| {
+            let nexcm_b = a_nexcm_b(a);
+            Rc::new(move |nb| nexcm_b(Right(nb)))
+        });
+    let na: Not<A> = Rc::new(move |a| a_nnb(a.clone())(a_nb(a)));
+    let na2 = na.clone();
+    let nb: Not<B> = Rc::new(move |b| na_nexcm_b(na2.clone())(Left(b)));
+    let eq: Eq<ExcM<A>, ExcM<B>> = (Right(nb).map_any(), Right(na).map_any());
+    catus(eq)
+}
+
+/// Proves that any Catuṣkoṭi with relative excluded middle of non-existence is absurd.
+///
+/// A Catuṣkoṭi is a 4-value logic.
+/// For more information, see https://en.wikipedia.org/wiki/Catu%E1%B9%A3ko%E1%B9%ADi
+pub fn catuskoti_e_absurd<A: Prop, B: Prop>(
+    catus: Not<Eq<E<A>, E<B>>>
+) -> False {
+    let catus2 = catus.clone();
+    let a_ne_b: Imply<A, Not<E<B>>> =
+        Rc::new(move |a| {
+            let catus2 = catus2.clone();
+            Rc::new(move |e_b| {
+                let eq = (e_b.map_any(), Left(not::double(a.clone())).map_any());
+                catus2.clone()(eq)
+            })
+        });
+    let catus2 = catus.clone();
+    let na_ne_b: Imply<Not<A>, Not<E<B>>> =
+        Rc::new(move |na| {
+            let catus2 = catus2.clone();
+            Rc::new(move |e_b| {
+                let eq = (e_b.map_any(), Right(na.clone()).map_any());
+                catus2.clone()(eq)
+            })
+        });
+    let a_ne_b2 = a_ne_b.clone();
+    let a_nb: Imply<A, Not<B>> =
+        Rc::new(move |a| {
+            let ne_b = a_ne_b2(a);
+            Rc::new(move |b| ne_b(Left(not::double(b))))
+        });
+    let a_nnb: Imply<A, Not<Not<B>>> =
+        Rc::new(move |a| {
+            let ne_b = a_ne_b(a);
+            Rc::new(move |nb| ne_b(Right(nb)))
+        });
+    let na: Not<A> = Rc::new(move |a| a_nnb(a.clone())(a_nb(a)));
+    let na2 = na.clone();
+    let nb: Not<B> = Rc::new(move |b| na_ne_b(na2.clone())(Left(not::double(b))));
+    let eq: Eq<E<A>, E<B>> = (Right(nb).map_any(), Right(na).map_any());
+    catus(eq)
+}
