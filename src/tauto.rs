49ffaf9e80ac144e586998488db809aa701ebff8
Sven Nilsen <bvssvni@gmail.com>
2022-09-05 04:14:43 +0200

    Improved naming

--- src/tauto.rs
+++ src/tauto.rs
@@ -59,35 +59,35 @@ pub fn consistency() -> Not<Tauto<False>> {
 }
 
 /// `a => (a == true)`.
-pub fn to_eq_true<A: Prop>(
+pub fn tauto_to_eq_true<A: Prop>(
     _: Tauto<A>
 ) -> Tauto<Eq<A, True>> {
     unimplemented!()
 }
 
 /// `(a == true) => a`.
-pub fn from_eq_true<A: Prop>(
+pub fn tauto_from_eq_true<A: Prop>(
     _: Tauto<Eq<A, True>>
 ) -> Tauto<A> {
     unimplemented!()
 }
 
 /// `¬a => (a == false)`.
-pub fn to_eq_false<A: Prop>(
+pub fn para_to_eq_false<A: Prop>(
     _: Para<A>
 ) -> Tauto<Eq<A, False>> {
     unimplemented!()
 }
 
 /// `(a == false) => ¬a`.
-pub fn from_eq_false<A: Prop>(
+pub fn para_from_eq_false<A: Prop>(
     _: Tauto<Eq<A, False>>
 ) -> Para<A> {
     unimplemented!()
 }
 
 /// `x => ¬¬x`.
-pub fn not_double<A: Prop>(_: Tauto<A>) -> Tauto<Not<Not<A>>> {
+pub fn tauto_not_double<A: Prop>(_: Tauto<A>) -> Tauto<Not<Not<A>>> {
     unimplemented!()
 }
 
@@ -105,31 +105,31 @@ pub fn para_not_rev_double<A: Prop>(_: Para<A>) -> Para<Not<Not<A>>> {
 pub use eq::refl as eq_refl;
 
 /// `((x == y) == true) => ((y == x) == true)`.
-pub fn eq_symmetry<A: Prop, B: Prop>(_: Tauto<Eq<A, B>>) -> Tauto<Eq<B, A>> {
+pub fn tauto_eq_symmetry<A: Prop, B: Prop>(_: Tauto<Eq<A, B>>) -> Tauto<Eq<B, A>> {
     unimplemented!()
 }
 
 /// `((x == y) == false) => ((y == x) == false)`.
-pub fn neq_symmetry<A: Prop, B: Prop>(_: Para<Eq<A, B>>) -> Para<Eq<B, A>> {
+pub fn para_eq_symmetry<A: Prop, B: Prop>(_: Para<Eq<A, B>>) -> Para<Eq<B, A>> {
     unimplemented!()
 }
 
 /// `(a == b) ∧ (b == c) => (a == c)`.
-pub fn eq_transitivity<A: Prop, B: Prop, C: Prop>(
+pub fn tauto_eq_transitivity<A: Prop, B: Prop, C: Prop>(
     _: Tauto<Eq<A, B>>,
     _: Tauto<Eq<B, C>>
 ) -> Tauto<Eq<A, C>> {
     unimplemented!()
 }
 
-pub use eq_transitivity as eq_in_right_arg;
+pub use tauto_eq_transitivity as tauto_eq_in_right_arg;
 
 /// `(a == b) ∧ (a == c) => (c == b)`.
-pub fn eq_in_left_arg<A: Prop, B: Prop, C: Prop>(
+pub fn tauto_eq_in_left_arg<A: Prop, B: Prop, C: Prop>(
     f: Tauto<Eq<A, B>>,
     g: Tauto<Eq<A, C>>,
 ) -> Tauto<Eq<C, B>> {
-    eq_transitivity(eq_symmetry(g), f)
+    tauto_eq_transitivity(tauto_eq_symmetry(g), f)
 }
 
 /// `((a == b) == false) ∧ ((b == c) == true) => ((a == c) == false)`.
@@ -149,13 +149,12 @@ pub fn para_eq_transitivity_right<A: Prop, B: Prop, C: Prop>(
 }
 
 /// `x => x`.
-pub fn imply_refl<A: Prop>() -> Tauto<Imply<A, A>> {
-    fn inner<A: Prop>() -> Imply<A, A> {Rc::new(move |x| x)}
-    inner::<A>
+pub fn imply_refl<A: Prop>() -> Imply<A, A> {
+    Rc::new(move |x| x)
 }
 
 /// `(a => b) ∧ (b => c) => (a => c)`.
-pub fn imply_transitivity<A: Prop, B: Prop, C: Prop>(
+pub fn tauto_imply_transitivity<A: Prop, B: Prop, C: Prop>(
     _: Tauto<Imply<A, B>>,
     _: Tauto<Imply<B, C>>,
 ) -> Tauto<Imply<A, C>> {
@@ -163,26 +162,26 @@ pub fn imply_transitivity<A: Prop, B: Prop, C: Prop>(
 }
 
 /// `(a ∧ b) => (a == b)`.
-pub fn and_to_eq_pos<A: Prop, B: Prop>(_: Tauto<A>, _: Tauto<B>) -> Tauto<Eq<A, B>> {
+pub fn tauto_and_to_eq_pos<A: Prop, B: Prop>(_: Tauto<A>, _: Tauto<B>) -> Tauto<Eq<A, B>> {
     unimplemented!()
 }
 
 /// `(a == true) => ((a ⋁ b) == true)`.
-pub fn or_left<A: Prop, B: Prop>(
+pub fn tauto_left_or<A: Prop, B: Prop>(
     _: Tauto<A>
 ) -> Tauto<Or<A, B>> {
     unimplemented!()
 }
 
 /// `(b == true) => ((a ⋁ b) == true)`.
-pub fn or_right<A: Prop, B: Prop>(
+pub fn tauto_right_or<A: Prop, B: Prop>(
     _: Tauto<B>
 ) -> Tauto<Or<A, B>> {
     unimplemented!()
 }
 
 /// `(a == false) ∧ (b == false) => ((a ⋁ b) == false)`.
-pub fn or_para<A: Prop, B: Prop>(
+pub fn para_to_or<A: Prop, B: Prop>(
     _: Para<A>,
     _: Para<B>
 ) -> Para<Or<A, B>> {
@@ -190,21 +189,21 @@ pub fn or_para<A: Prop, B: Prop>(
 }
 
 /// `((a ⋁ b) == false) => (a == false) ∧ (b == false)`.
-pub fn rev_or_para<A: Prop, B: Prop>(
+pub fn para_from_or<A: Prop, B: Prop>(
     _: Para<Or<A, B>>,
 ) -> And<Para<A>, Para<B>> {
     unimplemented!()
 }
 
 /// `((a ∧ b) == false) => (a == false) ⋁ (b == false)`.
-pub fn para_or<A: Prop, B: Prop>(
+pub fn para_and_to_or<A: Prop, B: Prop>(
     _: Para<And<A, B>>
 ) -> Or<Para<A>, Para<B>> {
     unimplemented!()
 }
 
 /// `(a == true) ∧ (b == true) => ((a ∧ b) == true)`.
-pub fn and<A: Prop, B: Prop>(
+pub fn tauto_and<A: Prop, B: Prop>(
     _: Tauto<A>,
     _: Tauto<B>,
 ) -> Tauto<And<A, B>> {
@@ -212,28 +211,28 @@ pub fn and<A: Prop, B: Prop>(
 }
 
 /// `((a ∧ b) == true) => (a == true) ∧ (b == true)`.
-pub fn rev_and<A: Prop, B: Prop>(
+pub fn tauto_rev_and<A: Prop, B: Prop>(
     _: Tauto<And<A, B>>,
 ) -> And<Tauto<A>, Tauto<B>> {
     unimplemented!()
 }
 
 /// `(a == false) => ((a ∧ b) == false)`.
-pub fn and_para_left<A: Prop, B: Prop>(
+pub fn para_left_and<A: Prop, B: Prop>(
     _: Para<A>,
 ) -> Para<And<A, B>> {
     unimplemented!()
 }
 
 /// `(b == false) => ((a ∧ b) == false)`.
-pub fn and_para_right<A: Prop, B: Prop>(
+pub fn para_right_and<A: Prop, B: Prop>(
     _: Para<B>,
 ) -> Para<And<A, B>> {
     unimplemented!()
 }
 
 /// `(a => b) ∧ (a == c)  =>  (c => b)`.
-pub fn imply_in_left_arg<A: Prop, B: Prop, C: Prop>(
+pub fn tauto_imply_in_left_arg<A: Prop, B: Prop, C: Prop>(
     _: Tauto<Imply<A, B>>,
     _: Tauto<Eq<A, C>>
 ) -> Tauto<Imply<C, B>> {
@@ -241,10 +240,18 @@ pub fn imply_in_left_arg<A: Prop, B: Prop, C: Prop>(
 }
 
 /// `(a => b) ∧ (b == c)  =>  (a => c)`.
-pub fn imply_in_right_arg<A: Prop, B: Prop, C: Prop>(
+pub fn tauto_imply_in_right_arg<A: Prop, B: Prop, C: Prop>(
     _: Tauto<Imply<A, B>>,
     _: Tauto<Eq<B, C>>
 ) -> Tauto<Imply<A, C>> {
+    unimplemented!()
+}
+
+/// `(a => b) ∧ a  =>  b`.
+pub fn tauto_modus_ponens<A: Prop, B: Prop>(
+    _: Tauto<Imply<A, B>>,
+    _: Tauto<A>,
+) -> Tauto<B> {
     unimplemented!()
 }
 
@@ -253,7 +260,7 @@ pub fn uniform_not_double<A: Prop>(
     f: Uniform<A>
 ) -> Uniform<Not<Not<A>>> {
     match f {
-        Left(x) => Left(not_double(x)),
+        Left(x) => Left(tauto_not_double(x)),
         Right(x) => Right(para_not_rev_double(x)),
     }
 }
@@ -268,8 +275,8 @@ pub fn uniform_symmetry<A: Prop, B: Prop>(
     f: Uniform<Eq<A, B>>
 ) -> Uniform<Eq<B, A>> {
     match f {
-        Left(t_ab) => Left(eq_symmetry(t_ab)),
-        Right(p_ab) => Right(neq_symmetry(p_ab)),
+        Left(t_ab) => Left(tauto_eq_symmetry(t_ab)),
+        Right(p_ab) => Right(para_eq_symmetry(p_ab)),
     }
 }
 
@@ -279,7 +286,7 @@ pub fn uniform_transitivity<A: Prop, B: Prop, C: Prop>(
     g: Uniform<Eq<B, C>>
 ) -> Uniform<Eq<A, C>> {
     match (f, g) {
-        (Left(t_ab), Left(t_bc)) => Left(eq_transitivity(t_ab, t_bc)),
+        (Left(t_ab), Left(t_bc)) => Left(tauto_eq_transitivity(t_ab, t_bc)),
         (Left(t_ab), Right(p_bc)) => Right(para_eq_transitivity_right(t_ab, p_bc)),
         (Right(p_ab), Left(t_bc)) => Right(para_eq_transitivity_left(p_ab, t_bc)),
         (Right(p_ab), Right(p_bc)) => uniform_from_para_transitivity(p_ab, p_bc),
@@ -303,7 +310,7 @@ pub fn uniform_and<A: Prop, B: Prop>(
 }
 
 /// `uniform(a ∧ b) => uniform(a) ∧ uniform(b)`.
-pub fn rev_uniform_and<A: Prop, B: Prop>(
+pub fn uniform_rev_and<A: Prop, B: Prop>(
     _: Uniform<And<A, B>>,
 ) -> And<Uniform<A>, Uniform<B>> {
     unimplemented!()
@@ -324,17 +331,9 @@ pub fn uniform_to_excm<A: Prop>(
 }
 
 /// `(a ⋁ ¬a) => uniform(a)`.
-pub fn excm_to_uniform<A: Prop>(
+pub fn uniform_from_excm<A: Prop>(
     _: Tauto<ExcM<A>>
 ) -> Uniform<A> {
-    unimplemented!()
-}
-
-/// `(a => b) ∧ a  =>  b`.
-pub fn modus_ponens<A: Prop, B: Prop>(
-    _: Tauto<Imply<A, B>>,
-    _: Tauto<A>,
-) -> Tauto<B> {
     unimplemented!()
 }
 
@@ -344,8 +343,8 @@ pub fn theory_and<A: Prop, B: Prop>(
     g: Theory<B>
 ) -> Theory<And<A, B>> {
     Rc::new(move |uni| match uni {
-        Left(t_ab) => f(Left(rev_and(t_ab).0)),
-        Right(p_ab) => match para_or(p_ab) {
+        Left(t_ab) => f(Left(tauto_rev_and(t_ab).0)),
+        Right(p_ab) => match para_and_to_or(p_ab) {
             Left(p_a) => f(Right(p_a)),
             Right(p_b) => g(Right(p_b)),
         }
