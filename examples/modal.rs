c94ba8a238c4bf35c4616ddbf99745be955ea874
Sven Nilsen <bvssvni@gmail.com>
2022-10-04 15:25:18 +0200

    Added "modal" example

---
+++ examples/modal.rs
@@ -0,0 +1,31 @@
+use prop::*;
+use modal::*;
+use hooo::*;
+
+// `¬¬¬◇a => ¬□a`.
+pub fn proof1<A: Prop>(nnnpos_a: Not<Not<Not<Pos<A>>>>) -> Not<Nec<A>> {
+    imply::in_left(nnnpos_a, |x: Nec<A>| {
+        let x = npos_to_para(x);
+        let x = pow_rev_not(x);
+        let x: Not<Not<Pos<A>>> = imply::in_left(x, npos_to_para);
+        x
+    })
+}
+
+// `¬◇a => ¬□a`.
+pub fn proof2<A: Prop>(npos_a: Not<Pos<A>>) -> Not<Nec<A>> {
+    proof1(not::double(npos_a))
+}
+
+/// `¬□a => ¬◇a`.
+pub unsafe fn proof3<A: Prop>(nnec_a: Not<Nec<A>>) -> Not<Pos<A>> {
+    let x = not::rev_triple(imply::in_left(nnec_a, |x: Not<Not<Pos<A>>>| {
+        let x: Not<Para<A>> = imply::in_left(x, |y| para_to_npos(y));
+        let x = pow_not(x);
+        let x = para_to_npos(x);
+        x
+    }));
+    x
+}
+
+fn main() {}
