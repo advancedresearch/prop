d5409a71bb6b1bc93ab3a0265b688e7449fa51e5
Sven Nilsen <bvssvni@gmail.com>
2022-10-04 10:26:29 +0200

    Updated code to new definition

--- src/modal.rs
+++ src/modal.rs
@@ -5,10 +5,10 @@
 use crate::*;
 use hooo::*;
 
-/// `□a`.
-type Nec<A> = Tauto<A>;
 /// `◇a`.
 type Pos<A> = Para<Para<A>>;
+/// `□a`.
+type Nec<A> = Not<Pos<Not<A>>>;
 
 /// `¬◇a => false^a`.
 pub fn npos_to_para<A: DProp>(npos: Not<Pos<A>>) -> Para<A> {
@@ -25,65 +25,97 @@ pub fn para_to_npos<A: Prop>(para_a: Para<A>) -> Not<Pos<A>> {
 
 /// `¬□¬a <=> ◇a`.
 pub fn eq_nnecn_pos<A: Prop>() -> Eq<Not<Nec<Not<A>>>, Pos<A>> {
+    fn f<A: Prop>(_: True) -> Eq<Not<Para<A>>, Not<Para<Not<Not<A>>>>> {
+        (
+            Rc::new(move |x| pow_rev_not(para_not_triple(pow_not(x)))),
+            Rc::new(move |x| pow_rev_not(para_not_rev_triple(pow_not(x))))
+        )
+    }
+    fn g<A: Prop>(_: True) -> Eq<Para<A>, Para<Not<Not<A>>>> {
+        (Rc::new(move |x| para_not_double(x)), Rc::new(move |x| para_not_rev_double(x)))
+    }
     (
-        Rc::new(move |n_nec_na| {
-            fn f<A: Prop>(para_a: Para<A>) -> Not<A> {
-                Rc::new(move |a| para_a(a))
-            }
-            let x: Pow<Not<Not<A>>, True> = hooo_rev_not()(n_nec_na);
-            let x: Pow<Para<Not<A>>, True> = pow_imply(x);
-            let x: Para<Not<A>> = x(True);
-            pow_transitivity(f, x)
-        }),
-        Rc::new(move |pos_a| {
-            Rc::new(move |nec_na| {
-                let x = pow_imply(nec_na);
-                pos_a(x(True))
-            })
-        }),
-    )
-}
-
-/// `□p => ¬◇¬p`.
-pub fn nec_to_nposn<A: Prop>(nec_a: Nec<A>) -> Not<Pos<Not<A>>> {
-    Rc::new(move |pos_na| {
-        let x = Rc::new(move |x| pos_na(x));
-        pow_not(x)(not::double(nec_a(True)))
-    })
-}
-
-/// `¬◇¬a <=> □a`.
-pub fn eq_nposn_nec<A: DProp>() -> Eq<Not<Pos<Not<A>>>, Nec<A>> {
-    (
-        Rc::new(move |n_pos_na| {
-            fn f<A: Prop>(_: True) -> Eq<Not<Para<Not<A>>>, Not<Not<Para<A>>>> {
-                (
-                    Rc::new(move |x: Not<Para<Not<A>>>| {
-                        imply::in_left(x, |y| pow_not(y))
-                    }),
-                    Rc::new(move |y: Not<Not<Para<A>>>| {
-                        imply::in_left(y, |x| pow_rev_not(x))
-                    })
-                )
-            }
-            match Tauto::<A>::decide() {
-                Left(tauto_a) => tauto_a,
-                Right(ntauto_a) => {
-                    let x = pow_not(n_pos_na);
-                    let x: Para<Not<Not<Para<A>>>> = para_in_arg(x, f::<A>);
-                    let y: Tauto<Para<A>> = pow_imply(hooo_rev_not()(ntauto_a));
-                    imply::absurd()(x(not::double(y(True))))
+        Rc::new(move |nnec_na| {
+            match Pos::<A>::decide() {
+                Left(pos_a) => pos_a,
+                Right(npos_a) => {
+                    let x = pow_not(npos_a);
+                    let x = para_in_arg(x, f);
+                    let x = pow_rev_not(x);
+                    imply::absurd()(nnec_na(x))
                 }
             }
         }),
-        Rc::new(move |nec_a| nec_to_nposn(nec_a)),
+        Rc::new(move |x| not::double(para_in_arg(x, g))),
     )
 }
 
-/// `¬□a == □¬a`.
-pub fn listing_mobius<A: Prop>() -> Eq<Not<Nec<A>>, Nec<Not<A>>> {
+/// `¬◇¬a <=> □a`.
+pub fn eq_nposn_nec<A: Prop>() -> Eq<Not<Pos<Not<A>>>, Nec<A>> {
+    eq::refl()
+}
+
+/// `¬□a == ◇¬a`.
+pub fn eq_nnec_posn<A: Prop>() -> Eq<Not<Nec<A>>, Pos<Not<A>>> {
+    fn g<A: Prop>(_: True) -> Eq<Not<Not<Para<Not<A>>>>, Para<Not<A>>> {
+        fn f<A: Prop>(_: True) -> Eq<Not<Not<Not<A>>>, Not<A>> {
+            (Rc::new(move |x| not::rev_triple(x)), Rc::new(move |x| not::double(x)))
+        }
+        (
+            Rc::new(move |x| {
+                let x = imply::in_left(x, |y| pow_rev_not(y));
+                para_in_arg(pow_not(x), f)
+            }),
+            Rc::new(move |x| {
+                let x = para_in_arg(x, tauto_eq_symmetry(f));
+                let x: Not<Para<Not<Not<A>>>> = pow_rev_not(x);
+                imply::in_left(x, |y| pow_not(y))
+            })
+        )
+    }
     (
-        Rc::new(move |nnec_a| hooo_rev_not()(nnec_a)),
-        Rc::new(move |nec_na| hooo_not()(nec_na)),
+        Rc::new(move |nnec_a: Not<Not<Para<Para<Not<A>>>>>| {
+            let x = imply::in_left(nnec_a, |y| pow_rev_not(y));
+            let x = pow_not(x);
+            para_in_arg(x, g)
+        }),
+        Rc::new(move |pos_na| {
+            let x: Para<Not<Not<Para<Not<A>>>>> = para_in_arg(pos_na, tauto_eq_symmetry(g));
+            let x = pow_rev_not(x);
+            imply::in_left(x, |y| pow_not(y))
+        })
+    )
+}
+
+/// `¬◇a == □¬a`.
+pub fn eq_posn_nnec<A: Prop>() -> Eq<Not<Pos<A>>, Nec<Not<A>>> {
+    fn f<A: Prop>(_: True) -> Eq<Not<Para<A>>, Not<Para<Not<Not<A>>>>> {
+        fn g<A: Prop>(_: True) -> Eq<Not<Not<Not<A>>>, Not<A>> {
+            (Rc::new(move |x| not::rev_triple(x)), Rc::new(move |x| not::double(x)))
+        }
+        (
+            Rc::new(move |x| {
+                let x = pow_not(x);
+                let x = para_in_arg(x, tauto_eq_symmetry(g));
+                pow_rev_not(x)
+            }),
+            Rc::new(move |x| {
+                let x = pow_not(x);
+                let x = para_in_arg(x, g);
+                pow_rev_not(x)
+            })
+        )
+    }
+    (
+        Rc::new(move |x| {
+            let x = pow_not(x);
+            let x = para_in_arg(x, f);
+            pow_rev_not(x)
+        }),
+        Rc::new(move |x| {
+            let x = pow_not(x);
+            let x = para_in_arg(x, tauto_eq_symmetry(f));
+            pow_rev_not(x)
+        })
     )
 }
