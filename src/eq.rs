fbe18d89b48fdd090535f5802b85e3ba4051b137
Sven Nilsen <bvssvni@gmail.com>
2022-11-04 01:14:18 +0100

    Added `eq::rev_modus_tollens_imply_excm`

--- src/eq.rs
+++ src/eq.rs
@@ -90,6 +90,17 @@ pub fn rev_modus_tollens_eq_excm<A: Prop, B: Prop>(
     (f02, f12)
 }
 
+/// `(¬a == ¬b) ∧ (a => (b ∨ ¬b)) ∧ (b => (b ∨ ¬b)) => (b == a)`.
+pub fn rev_modus_tollens_imply_excm<A: Prop, B: Prop>(
+    (f0, f1): Eq<Not<A>, Not<B>>,
+    a_excm_b: Imply<A, ExcM<B>>,
+    b_excm_a: Imply<B, ExcM<A>>,
+) -> Eq<B, A> {
+    let f02 = imply::rev_modus_tollens_imply_excm(f0, b_excm_a);
+    let f12 = imply::rev_modus_tollens_imply_excm(f1, a_excm_b);
+    (f02, f12)
+}
+
 /// `(true = a) => a`.
 pub fn is_true<A: Prop>((f0, _): Eq<True, A>) -> A {
     f0(True)
