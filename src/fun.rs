16496c7f8fdcabe18d0057ade67dfbfbef86b73a
Sven Nilsen <bvssvni@gmail.com>
2023-01-17 23:49:39 +0100

    Renamed `fun::pow_` to `fun::fun_`

--- src/fun.rs
+++ src/fun.rs
@@ -455,12 +455,12 @@ pub fn type_imply<N: Nat>(Type(n): Type<N>) -> Type<S<N>> {Type(S(n))}
 /// `is_const(type(n))`.
 pub fn type_is_const<N: Nat>() -> IsConst<Type<N>> {unimplemented!()}
 /// `(a : x) ⋀ (b : y)  =>  (a -> b) : (x -> y)`.
-pub fn pow_ty<A: Prop, B: Prop, X: Prop, Y: Prop>(
+pub fn fun_ty<A: Prop, B: Prop, X: Prop, Y: Prop>(
     _: Ty<A, X>,
     _: Ty<B, Y>
 ) -> Ty<Pow<B, A>, Pow<Y, X>> {unimplemented!()}
 /// `(type(n) -> type(m)) : type(0)`.
-pub fn pow_type_ty<N: Nat, M: Nat>() -> Ty<Pow<Type<M>, Type<N>>, Type<Z>> {unimplemented!()}
+pub fn fun_type_ty<N: Nat, M: Nat>() -> Ty<Pow<Type<M>, Type<N>>, Type<Z>> {unimplemented!()}
 /// `(b : type(n))  =>  (a : b) : type(n)`.
 pub fn judgement_ty<A: Prop, B: Prop, N: Nat>(_ty_b: Ty<B, Type<N>>) -> Ty<Ty<A, B>, Type<N>> {
     unimplemented!()
@@ -481,10 +481,10 @@ pub fn type_ty<N: Nat>() -> Ty<Type<N>, Type<S<N>>> {
     (hooo::pow_to_imply(type_imply), POrdProof::new())
 }
 /// `(a : type(n)) ⋀ (b : type(m))  =>  (a -> b) : type(0)`.
-pub fn pow_type0<A: Prop, B: Prop, N: Nat, M: Nat>(
+pub fn fun_type0<A: Prop, B: Prop, N: Nat, M: Nat>(
     ty_a: Ty<A, Type<N>>,
     ty_b: Ty<B, Type<M>>
-) -> Ty<Pow<B, A>, Type<Z>> {path_semantics::ty_transitivity(pow_ty(ty_a, ty_b), pow_type_ty())}
+) -> Ty<Pow<B, A>, Type<Z>> {path_semantics::ty_transitivity(fun_ty(ty_a, ty_b), fun_type_ty())}
 /// `(f : A -> B) ⋀ (inv(f) ~~ g) => ((f ~~ g) : ((A -> B) ~~ (B -> A)))`.
 pub fn q_inv_ty<F: Prop, G: Prop, A: Prop, B: Prop>(
     ty_f: Ty<F, Pow<B, A>>,
@@ -866,7 +866,7 @@ pub fn dep_fun_ty_formation<A: Prop, X: Prop, P: Prop>(
     fn f<A: Prop, B: Prop, X: Prop, Y: Prop>((x, y): And<Ty<A, X>, Pow<Ty<B, Y>, A>>) ->
         Ty<Pow<B, A>, Pow<Y, X>> {dep_fun_ty(x, y)}
     fn g<A: Prop, B: Prop>(x: Ty<Pow<B, A>, Pow<Type<Z>, Type<Z>>>) -> Ty<Pow<B, A>, Type<Z>> {
-        path_semantics::ty_transitivity(x, pow_type_ty())
+        path_semantics::ty_transitivity(x, fun_type_ty())
     }
     hooo_rev_and((ty_x.trans(judgement_ty), pow_lift(pow_ty_pa_ty_a))).trans(f).trans(g)
 }
