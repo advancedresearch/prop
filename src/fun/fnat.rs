f84eb32a3cc52d5289d4443c0468aeaabf610281
Sven Nilsen <bvssvni@gmail.com>
2023-01-20 12:02:52 +0100

    Added `fun::fnat`

---
+++ src/fun/fnat.rs
@@ -0,0 +1,24 @@
+//! Natural numbers.
+
+use super::*;
+
+/// The type of natural numbers.
+#[derive(Copy, Clone)]
+pub struct Nat(());
+
+/// `nat : type(0)`.
+pub fn nat_ty() -> Ty<Nat, Type<Z>> {unimplemented!()}
+
+/// Zero.
+#[derive(Copy, Clone)]
+pub struct Zero(());
+
+/// `zero : nat`.
+pub fn zero_ty() -> Ty<Zero, Type<Z>> {unimplemented!()}
+
+/// Successor.
+#[derive(Copy, Clone)]
+pub struct Succ(());
+
+/// `succ : nat -> nat`.
+pub fn succ_ty() -> Ty<Succ, Pow<Nat, Nat>> {unimplemented!()}
