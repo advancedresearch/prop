d3b34c11566ca8a0a9588c9a11206fb5dc732d28
Sven Nilsen <bvssvni@gmail.com>
2023-01-20 12:21:11 +0100

    Added `fun::fin::Inc`

--- src/fun/fin.rs
+++ src/fun/fin.rs
@@ -1,7 +1,7 @@
 //! Finite sets.
 
 use super::*;
-use fnat::{Nat, Succ, Zero};
+use fnat::{Nat, Inc, Zero};
 
 /// A finite set.
 #[derive(Copy, Clone)]
@@ -15,12 +15,12 @@ pub fn fin_ty() -> Ty<Fin, Pow<Type<Zero>, Nat>> {unimplemented!()}
 pub struct Empty(());
 
 /// `(n : nat)  =>  (empty : fin(n + 1))`.
-pub fn empty_ty<N>(_n_ty: Ty<N, Nat>) -> Ty<Empty, App<Fin, App<Succ, N>>> {unimplemented!()}
+pub fn empty_ty<N>(_n_ty: Ty<N, Nat>) -> Ty<Empty, App<Fin, Inc<N>>> {unimplemented!()}
 
 /// A finite set.
 #[derive(Copy, Clone)]
 pub struct FinSucc(());
 
 /// `(n : nat)  =>  fin_succ : fin(n) -> fin(n + 1)`.
-pub fn fin_succ_ty<N>(_n_ty: Ty<N, Nat>) -> Ty<FinSucc, Pow<App<Fin, App<Succ, N>>, App<Fin, N>>>
+pub fn fin_succ_ty<N>(_n_ty: Ty<N, Nat>) -> Ty<FinSucc, Pow<App<Fin, Inc<N>>, App<Fin, N>>>
 {unimplemented!()}
