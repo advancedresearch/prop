dad9c197eb0dd3709cbafc55a8ddbc304c023475
Sven Nilsen <bvssvni@gmail.com>
2023-02-13 14:41:58 +0100

    Added `fun::fnat::add_symmetry`

--- src/fun/fnat.rs
+++ src/fun/fnat.rs
@@ -159,6 +159,8 @@ pub fn add_succ<N: Prop, M: Prop>(
     _ty_n: Ty<N, Nat>,
     _ty_m: Ty<M, Nat>
 ) -> Eq<Plus<Inc<N>, M>, Inc<Plus<N, M>>> {unimplemented!()}
+/// `add(n, m) == add(m, n)`.
+pub fn add_symmetry<N: Prop, M: Prop>() -> Eq<Plus<N, M>, Plus<M, N>> {unimplemented!()}
 /// `(n : nat)  =>  succ(n) == n + 1`.
 pub fn add_succ_plus_one<N: Prop>(_ty_n: Ty<N, Nat>) -> Eq<Inc<N>, Plus<N, One>> {unimplemented!()}
 /// `(n : nat)  =>  add(n, 0) == n`.
