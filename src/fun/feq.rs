75d233464e5866a91082968165fc9779c1b292d9
Sven Nilsen <bvssvni@gmail.com>
2023-02-23 17:31:20 +0100

    Fixed comments

--- src/fun/feq.rs
+++ src/fun/feq.rs
@@ -13,19 +13,19 @@ pub fn eq_ty<T: Prop>(_ty_t: Ty<T, Type<Z>>) -> Ty<App<FEq, T>, Pow<Bool, Tup<T,
 pub fn implicit_eq_is_const() -> IsConst<FEq> {unimplemented!()}
 /// `is_const(eq{t})`.
 pub fn eq_is_const<T: Prop>(_: IsConst<T>) -> IsConst<App<FEq, T>> {unimplemented!()}
-/// `a : x  =>  eq{x}((a, a)) = true`.
+/// `a : x  =>  eq{x}(a, a) = true`.
 pub fn eq_refl<X: Prop, A: Prop>(
     _ty_a: Ty<A, X>
 ) -> Eq<App<App<FEq, X>, Tup<A, A>>, Tr> {unimplemented!()}
 
-/// `(a == b)  =>  eq{a}((a, b)) = true`.
+/// `(a : x) ⋀ (a == b)  =>  eq{x}(a, b) = true`.
 pub fn eq_lift<X: Prop, A: Prop, B: Prop>(
     ty_a: Ty<A, X>,
     x: Eq<A, B>
 ) -> Eq<App<App<FEq, X>, Tup<A, B>>, Tr> {
     eq::eq_left(app_eq(tup_eq_snd(x))).0(eq_refl(ty_a))
 }
-/// `eq{x}((a, b)) = false  =>  ¬(a == b)`.
+/// `eq{x}(a, b) = false  =>  ¬(a == b)`.
 pub fn eq_fa_lower<X: Prop, A: Prop, B: Prop>(
     ty_a: Ty<A, X>,
     x: Eq<App<App<FEq, X>, Tup<A, B>>, Fa>
